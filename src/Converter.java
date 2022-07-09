319441889621e3ff2005c227fddbe97fa91e1064
Nikita Komissarov <komissarov.na@yandex.ru>
2022-07-09 17:43:50 +0300

    Added a few more fixes

--- src/Converter.java
+++ src/Converter.java
@@ -3,115 +3,114 @@ import java.util.Map;
 import java.util.Set;
 
 class Converter {
-    private static final HashMap<Integer, Character> tableview = new HashMap<>() {
-        {
-            put(1, 'I');
-            put(5, 'V');
-            put(10, 'X');
-        }
-    };
+	private static final HashMap<Integer, Character> tableview = new HashMap<>() {
+		{
+			put(1, 'I');
+			put(5, 'V');
+			put(10, 'X');
+		}
+	};
 
-    public boolean roman(String string) {
-        return string.matches("[IVX]*");
-    }
+	public boolean roman(String string) {
+		return string.matches("[IVX]*");
+	}
 
-    public String convertToRoman(int number) throws Exception {
-        if (number > 39) {
-            throw new Exception("Калькулятор принимает числа от 1 до 10 включительно");
-        }
-        String output = "";
-        int i = number % 10;
-        int j = number % 100 - i;
+	public String convertToRoman(int number) throws Exception {
+		if (number > 39) {
+			throw new Exception("Калькулятор принимает числа от 1 до 10 включительно");
+		}
+		String output = "";
+		int i = number % 10;
+		int j = number % 100 - i;
 
-        output += romanUtils(j);
-        output += romanUtils(i);
-        return (output);
-    }
+		output += romanUtils(j);
+		output += romanUtils(i);
+		return (output);
+	}
 
-    private String romanUtils(int num) {
-        StringBuilder output = new StringBuilder();
+	private String romanUtils(int num) {
+		StringBuilder output = new StringBuilder();
 
-        if (num == 0) {
-            return output.toString();
-        }
-        int multipl;
-        if (num / 10 == 0) {
-            multipl = 1;
-        } else {
-            multipl = 10;
-        }
-        if (num < 9 * multipl) {
-            if (num < 5 * multipl) {
-                if (num != 4 * multipl) {
-                    for (int i = 0; i < num / multipl; i++) {
-                        output.append(tableview.get(multipl));
-                    }
-                } else {
-                    output.append(tableview.get(multipl));
-                    output.append(tableview.get(multipl * 5));
-                }
-            } else {
-                output.append(tableview.get(multipl * 5));
-                for (int i = 0; i < (num - 5 * multipl) / multipl; i++) {
-                    output.append(tableview.get(multipl));
-                }
-            }
-        } else {
-            output.append(tableview.get(multipl));
-            output.append(tableview.get(multipl * 10));
-        }
-        return (output.toString());
-    }
-// I + III + XXL
-    public Integer convertToArabic(String toConvert) throws Exception {
-        int output = 0;
+		if (num == 0) {
+			return output.toString();
+		}
+		int multipl;
+		if (num / 10 == 0) {
+			multipl = 1;
+		} else {
+			multipl = 10;
+		}
+		if (num < 9 * multipl) {
+			if (num < 5 * multipl) {
+				if (num != 4 * multipl) {
+					for (int i = 0; i < num / multipl; i++) {
+						output.append(tableview.get(multipl));
+					}
+				} else {
+					output.append(tableview.get(multipl));
+					output.append(tableview.get(multipl * 5));
+				}
+			} else {
+				output.append(tableview.get(multipl * 5));
+				for (int i = 0; i < (num - 5 * multipl) / multipl; i++) {
+					output.append(tableview.get(multipl));
+				}
+			}
+		} else {
+			output.append(tableview.get(multipl));
+			output.append(tableview.get(multipl * 10));
+		}
+		return (output.toString());
+	}
 
-        char[] string = toConvert.toCharArray();
-        if (string.length == 3) {
-            int num1 = getKey(string[0]);
-            int num2 = getKey(string[1]);
-            int num3 = getKey(string[2]);
-            if (num3 > num2) {
-                output = num3 - num2 - num1;
-            }
+	public Integer convertToArabic(String toConvert) throws Exception {
+		int output = 0;
+
+		char[] string = toConvert.toCharArray();
+		if (string.length == 3) {
+			int num1 = getKey(string[0]);
+			int num2 = getKey(string[1]);
+			int num3 = getKey(string[2]);
+			if (num3 > num2) {
+				output = num3 - num2 - num1;
+			}
 			else {
-                output = num1 + num2 + num3;
-            }
-        }
-        else if (string.length == 2) {
-            int num1 = getKey(string[0]);
-            int num2 = getKey(string[1]);
-            if (num1 > num2) {
-                output = num1 + num2;
-            } 
+				output = num1 + num2 + num3;
+			}
+		}
+		else if (string.length == 2) {
+			int num1 = getKey(string[0]);
+			int num2 = getKey(string[1]);
+			if (num1 > num2) {
+				output = num1 + num2;
+			} 
 			else if (num1 < num2) {
 				output = num2 - num1;
 			}
 			else {
-                output = num1 + num2;
-            }
-        }
-        else if (string.length == 1) {
-            int num = getKey(string[0]);
-            output = num;
-        }
-        if (output != 0) {
-            return (output);
-        }
+				output = num1 + num2;
+			}
+		}
+		else if (string.length == 1) {
+			int num = getKey(string[0]);
+			output = num;
+		}
+		if (output != 0) {
+			return (output);
+		}
 		else {
 			throw new Exception("Калькулятор принимает числа от 1 до 10 включительно");
 		}
-        // return (output);
-    }
+	}
 
-    private Integer getKey(Character num) {
-        Set<Map.Entry<Integer, Character>> set = tableview.entrySet();
+	private Integer getKey(Character num) {
+		Set<Map.Entry<Integer, Character>> set = tableview.entrySet();
 
-        for (Map.Entry<Integer, Character> entry : set) {
-            if (entry.getValue() == num) {
-                return entry.getKey();
-            }
-        }
-        return (-1);
-    }
+		for (Map.Entry<Integer, Character> entry : set) {
+			if (entry.getValue() == num) {
+				return entry.getKey();
+			}
+		}
+		return (-1);
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,76 +3,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.println(calc(br.readLine()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public static String calc(String input) throws Exception {
-        Converter converter = new Converter();
-        String output;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println(calc(br.readLine()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static String calc(String input) throws Exception {
+		Converter converter = new Converter();
+		String output;
 
-        if (input == null || input.equals("")) {
-            throw new Exception("Нет входных данных");
-        }
-        String[] inputs = input.split(" ");
-        if (inputs.length > 3) {
-            throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        }
-        if (inputs.length < 3) {
-            throw new Exception("Строка не является математической операцией");
-        }
+		if (input == null || input.equals("")) {
+			throw new Exception("Нет входных данных");
+		}
+		String[] inputs = input.split(" ");
+		if (inputs.length > 3) {
+			throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+		}
+		if (inputs.length < 3) {
+			throw new Exception("Строка не является математической операцией");
+		}
 		int num1 = parse(inputs[0]);
 		int num2 = parse(inputs[2]);
 		if (num1 > 10 || num2 > 10 || num1 < 1 || num2 < 1) {
-            throw new Exception("Калькулятор принимает числа от 1 до 10 включительно");
-        }
+			throw new Exception("Калькулятор принимает числа от 1 до 10 включительно");
+		}
 		else if (converter.roman(inputs[0]) ^ converter.roman(inputs[2])) {
-            throw new Exception("Нельзя использовать римские и арабские числа одновременно");
-        }
+			throw new Exception("Нельзя использовать римские и арабские числа одновременно");
+		}
 		else if (converter.roman(inputs[0]) || converter.roman(inputs[2])) {
-            if (inputs[1].equals("-") && (num1 < num2)) {
-                throw new Exception("В римской системе нет отрицательных чисел");
-            } else if (inputs[1].equals("-") && (num1 == num2)) {
+			if (inputs[1].equals("-") && (num1 < num2)) {
+				throw new Exception("В римской системе нет отрицательных чисел");
+			} else if (inputs[1].equals("-") && (num1 == num2)) {
 				throw new Exception("В римской системе цифр отсутствует ноль");
 			}
-            output = converter.convertToRoman(parse(calculate(num1, inputs[1], num2)));
-        }
-        else {
-            output = calculate(num1, inputs[1], num2);
-        }
-        return (output);
-    }
+			output = converter.convertToRoman(parse(calculate(num1, inputs[1], num2)));
+		}
+		else {
+			output = calculate(num1, inputs[1], num2);
+		}
+		return (output);
+	}
 
-    static String calculate(Integer num1, String op, Integer num2) throws Exception {
-        String output = "";
+	static String calculate(Integer num1, String op, Integer num2) throws Exception {
+		String output = "";
 
-        switch (op) {
-            case "+" -> output = String.valueOf(num1 + num2);
-            case "-" -> output = String.valueOf(num1 - num2);
-            case "*" -> output = String.valueOf(num1 * num2);
-            case "/" -> output = String.valueOf(num1 / num2);
-            default -> throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        }
-        return (output);
-    }
+		switch (op) {
+			case "+" -> output = String.valueOf(num1 + num2);
+			case "-" -> output = String.valueOf(num1 - num2);
+			case "*" -> output = String.valueOf(num1 * num2);
+			case "/" -> output = String.valueOf(num1 / num2);
+			default -> throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+		}
+		return (output);
+	}
 
-    static Integer parse(String string) throws Exception {
-        Integer output = null;
+	static Integer parse(String string) throws Exception {
+		Integer output = null;
 
-        try {
-            output = Integer.parseInt(string);
-        } catch (NumberFormatException e) {
-            if (new Converter().roman(string)) {
-                output = new Converter().convertToArabic(string);
-            }
-        }
-        if (output == null) {
-            throw new Exception("Неверный формат входных данных");
-        }
-        return (output);
-    }
+		try {
+			output = Integer.parseInt(string);
+		} catch (NumberFormatException e) {
+			if (new Converter().roman(string)) {
+				output = new Converter().convertToArabic(string);
+			}
+		}
+		if (output == null) {
+			throw new Exception("Неверный формат входных данных");
+		}
+		return (output);
+	}
 }
