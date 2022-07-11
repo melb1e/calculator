f27ec6b9a6ac7e1299fcb47e490ef8c7590bb67e
Nikita Komissarov <komissarov.na@yandex.ru>
2022-07-11 22:01:17 +0300

    Multiplication now works properly

--- src/Converter.java
+++ src/Converter.java
@@ -8,21 +8,26 @@ class Converter {
 			put(1, 'I');
 			put(5, 'V');
 			put(10, 'X');
+			put(50, 'L');
+			put(100, 'C');
+			put(500, 'D');
 		}
 	};
 
 	public boolean roman(String string) {
-		return string.matches("[IVX]*");
+		return string.matches("[IVXLCD]+");
 	}
 
 	public String convertToRoman(int number) throws Exception {
-		if (number > 39) {
+		if (number > 100) {
 			throw new Exception("Калькулятор принимает числа от 1 до 10 включительно");
 		}
 		String output = "";
 		int i = number % 10;
 		int j = number % 100 - i;
+		int k = number % 1000 - j - i;
 
+		output += romanUtils(k);
 		output += romanUtils(j);
 		output += romanUtils(i);
 		return (output);
@@ -35,10 +40,14 @@ class Converter {
 			return output.toString();
 		}
 		int multipl;
-		if (num / 10 == 0) {
-			multipl = 1;
+		if (num / 100 == 0) {
+			if (num / 10 == 0) {
+				multipl = 1;
+			} else {
+				multipl = 10;
+			}
 		} else {
-			multipl = 10;
+			multipl = 100;
 		}
 		if (num < 9 * multipl) {
 			if (num < 5 * multipl) {
